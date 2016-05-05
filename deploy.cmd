2fefb11a11acd26e437172195ccf5608bb908c60
Loïc Denuzière <loic@denuziere.net>
2016-05-05 09:47:20 +0200

    Unzip into the deployment directory

--- deploy.cmd
+++ deploy.cmd
@@ -4,7 +4,7 @@ echo ====== Downloading host service... ======
 curl http://test.intellifactory.com/host.zip -L -o host.zip
 
 echo ====== Unzipping... ======
-unzip -o -q host.zip
+unzip -o -q host.zip -d "%DEPLOYMENT_TARGET%"
 
 echo ====== Deleting zip file... ======
 del host.zip /q
